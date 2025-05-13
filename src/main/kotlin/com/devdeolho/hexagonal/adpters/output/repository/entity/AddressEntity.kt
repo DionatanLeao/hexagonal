33c04266ad13e13da7934a22a2c0376c8149cd65
DionatanLeao <dionatan.ti@outlook.com>
2025-05-12 22:10:43 -0300

    feat: Creating the Client Insert Adapter

--- src/main/kotlin/com/devdeolho/hexagonal/adpters/output/repository/entity/AddressEntity.kt
+++ src/main/kotlin/com/devdeolho/hexagonal/adpters/output/repository/entity/AddressEntity.kt
@@ -1,7 +1,16 @@
 package com.devdeolho.hexagonal.adpters.output.repository.entity
 
+import com.devdeolho.hexagonal.application.core.domain.Address
+
 class AddressEntity(
     val street: String,
     val city: String,
     val state: String
-)
+) {
+
+    constructor(address: Address) : this(
+        address.street,
+        address.city,
+        address.state
+    )
+}
