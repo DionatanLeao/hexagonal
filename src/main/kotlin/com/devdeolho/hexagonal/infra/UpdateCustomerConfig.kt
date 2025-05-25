9ff77d4e843b1874a1c0f33f443b32eed0ca75a1
DionatanLeao <dionatan.ti@outlook.com>
2025-05-25 17:23:36 -0300

    feat: Configuring the bean

---
+++ src/main/kotlin/com/devdeolho/hexagonal/infra/UpdateCustomerConfig.kt
@@ -0,0 +1,23 @@
+package com.devdeolho.hexagonal.infra
+
+import com.devdeolho.hexagonal.adpters.output.FindAddressByZipCodeAdapter
+import com.devdeolho.hexagonal.adpters.output.UpdateCustomerAdapter
+import com.devdeolho.hexagonal.application.core.usecase.FindCustomerByIdUseCase
+import com.devdeolho.hexagonal.application.core.usecase.UpdateCustomerUseCase
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class UpdateCustomerConfig {
+
+    @Bean
+    fun updateCustomer(
+        findCustomerByIdUseCase: FindCustomerByIdUseCase,
+        findAddressByZipCodeAdapter: FindAddressByZipCodeAdapter,
+        updateCustomerAdapter: UpdateCustomerAdapter
+    ) = UpdateCustomerUseCase(
+        findCustomerByIdUseCase,
+        findAddressByZipCodeAdapter,
+        updateCustomerAdapter
+    )
+}
