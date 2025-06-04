af474b929107b8edf3467a4727b751aa3719f30a
DionatanLeao <dionatan.ti@outlook.com>
2025-06-04 19:06:40 -0300

    feat: Creating the usecase

---
+++ src/main/kotlin/com/devdeolho/hexagonal/application/ports/output/FindAddressByZipCodeOutputPort.kt
@@ -0,0 +1,8 @@
+package com.devdeolho.hexagonal.application.ports.output
+
+import com.devdeolho.hexagonal.application.core.domain.Address
+
+interface FindAddressByZipCodeOutputPort {
+
+    fun find(zipCode: String): Address
+}
