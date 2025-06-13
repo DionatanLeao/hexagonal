d68d516d1ef33074cae6a7b6272f6f57a6b2c130
DionatanLeao <dionatan.ti@outlook.com>
2025-06-12 21:02:33 -0300

    feat: Creating the usecase

---
+++ src/main/kotlin/com/devdeolho/hexagonal/application/core/usecase/UpdateCustomerUpdate.kt
@@ -0,0 +1,24 @@
+package com.devdeolho.hexagonal.application.core.usecase
+
+import com.devdeolho.hexagonal.application.core.domain.Customer
+import com.devdeolho.hexagonal.application.ports.input.FindCustomerByIdInputPort
+import com.devdeolho.hexagonal.application.ports.output.FindAddressByZipCodeOutputPort
+import com.devdeolho.hexagonal.application.ports.output.UpdateCustomerOutputPort
+
+class UpdateCustomerUpdate(
+    private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
+    private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
+    private val updateCustomerOutputPort: UpdateCustomerOutputPort
+) {
+
+    fun update(customer: Customer, zipCode: String) {
+        if (customer.id == null) throw IllegalArgumentException("The id field cannot be null")
+        findCustomerByIdInputPort.find(customer.id)
+
+        customer.apply {
+            address = findAddressByZipCodeOutputPort.find(zipCode)
+        }.let {
+            updateCustomerOutputPort.update(it)
+        }
+    }
+}
