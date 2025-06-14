01fded7fd04dedf79050330fd65f98fa1e44e808
DionatanLeao <dionatan.ti@outlook.com>
2025-06-14 20:03:26 -0300

    feat: Creating the adapter

---
+++ src/main/kotlin/com/devdeolho/hexagonal/adapters/output/DeleteCustomerByIdAdapter.kt
@@ -0,0 +1,17 @@
+package com.devdeolho.hexagonal.adapters.output
+
+import com.devdeolho.hexagonal.adapters.output.repository.CustomerRepository
+import com.devdeolho.hexagonal.application.ports.output.DeleteCustomerByIdOutputPort
+import org.springframework.stereotype.Component
+
+@Component
+class DeleteCustomerByIdAdapter(
+    private val customerRepository: CustomerRepository
+): DeleteCustomerByIdOutputPort {
+
+    override fun delete(id: String) {
+        customerRepository.deleteById(id)
+    }
+
+
+}
